8462c4a880a2638c843d5d6557c8b68f227a656a
waquas9248 <55626672+waquas9248@users.noreply.github.com>
2024-02-11 20:49:47 -0600

    Update PetTypeFormatter.java

--- src/main/java/org/springframework/samples/petclinic/owner/PetTypeFormatter.java
+++ src/main/java/org/springframework/samples/petclinic/owner/PetTypeFormatter.java
@@ -51,10 +51,12 @@ public class PetTypeFormatter implements Formatter<PetType> {
 	@Override
 	public PetType parse(String text, Locale locale) throws ParseException {
 		Collection<PetType> findPetTypes = this.owners.findPetTypes();
-		for (PetType type : findPetTypes) {
-			if (type.getName().equals(text)) {
+		PetType type = null;
+		for(int i = 0; i < findPetTypes.size() ; i++){
+				type = findPetTypes.get(i);
+				if (type.getName().equals(text)) {
 				return type;
-			}
+			}		
 		}
 		throw new ParseException("type not found: " + text, 0);
 	}
